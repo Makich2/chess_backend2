2552a1428c8930c680549bd3c4f947d78b8c8f34
Rustam <waderprooo@gmail.com>
2023-11-03 20:42:09 +0300

    Добавил в models в класс MyUser пару дополнений

--- chess/chessbackend/models.py
+++ chess/chessbackend/models.py
@@ -6,12 +6,17 @@ from django.db import models
 
 
 class MyUser(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=500, unique=True)
-    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=70, unique=True)
+    login = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=255)
+    custom_id = models.AutoField(primary_key=True)
+    registration_date = models.DateField()
+    # achievments = models.
 
     def save(self, *args, **kwargs):
         # Хешируем пароль перед сохранением
         self.password = make_password(self.password)
         super(MyUser, self).save(*args, **kwargs)
+
+
+class
